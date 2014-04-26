bf8f486c455749cd42b7983836ad953f1f14d54e
Reuben Cummings <reubano@gmail.com>
2014-04-26 11:24:13 +0300

    Refactor kwargs

--- app/api.py
+++ app/api.py
@@ -8,7 +8,7 @@ from amazon.api import AmazonAPI
 class Amazon(AmazonAPI):
 	"""An Amazon search"""
 
-	def __init__(self, region='US', key=None, secret=None, tag=None, **kwargs):
+	def __init__(self, region='US', **kwargs):
 		"""
 		Initialization method.
 
@@ -33,9 +33,9 @@ class Amazon(AmazonAPI):
 		>>> Amazon()  #doctest: +ELLIPSIS
 		<app.api.Amazon object at 0x...>
 		"""
-		key = (key or getenv('AWS_ACCESS_KEY_ID'))
-		secret = (secret or getenv('AWS_SECRET_ACCESS_KEY'))
-		tag = (tag or getenv('AWS_ASSOCIATE_TAG'))
+		key = kwargs.pop('key', getenv('AWS_ACCESS_KEY_ID'))
+		secret = kwargs.pop('secret', getenv('AWS_SECRET_ACCESS_KEY'))
+		tag = kwargs.pop('tag', getenv('AWS_ASSOCIATE_TAG'))
 
 		if not (key and secret and tag):
 			raise SystemExit('Error getting Amazon credentials.')
